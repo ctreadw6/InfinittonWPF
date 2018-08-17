a2da33c43bfe742cbaba8ba48b06163eb8320754
ctreadw6 <ctreadw6@vols.utk.edu>
2018-08-16 21:16:21 -0400

    Made the app a SingleInstance app.

--- InfinittonWPF/App.xaml.cs
+++ InfinittonWPF/App.xaml.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Shell;
 
 namespace InfinittonWPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string Unique = "{28B3DD06-F3EC-4166-9363-3EFF7181E62C}";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // Bring window to foreground
+            if (this.MainWindow.WindowState == WindowState.Minimized)
+            {
+                this.MainWindow.WindowState = WindowState.Normal;
+            }
+
+            this.MainWindow.Activate();
+
+            return true;
+        }
+        #endregion
     }
 }
